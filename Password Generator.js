613c7bacd00ab177fd43cf29b51caf24a14f615d
mtabarikasif <79154025+m-tabarik@users.noreply.github.com>
2024-02-26 10:24:57 +0500

    Update Password Generator.js

--- Password Generator.js
+++ Password Generator.js
@@ -6,7 +6,7 @@ var password=document.getElementById("password");
     var password = "";
  for (var i = 0; i <= passwordLength; i++) {
    var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
+   password += chars.substring(randomNumber, randomNumber +1); 
   }
         document.getElementById("password").value = password;
  }
